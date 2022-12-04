cd38839d7e12f45de0a7dada20ec728a831d84d7
kanagapriya17 <113163018+kanagapriya17@users.noreply.github.com>
2022-12-04 18:35:04 +0530

    Update conn.js

--- conn.js
+++ conn.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 
-const dburl='mongodb+srv://priya:kanagapriyaclarit@cluster0.obudofj.mongodb.net/mernstack?retryWrites=true&w=majority';
+const dburl='mongodb+srv://velangudi:pMeBjqVHwONeinEV@cluster0.obudofj.mongodb.net/clarit?retryWrites=true&w=majority';
 
 const connectionParams={
     useNewUrlParser:true,   
